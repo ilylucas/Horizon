6d51f1eed1c410c9032aabaf26dd714bcca403d7
AzureGP <lucasribeiroo2010@gmail.com>
2023-07-25 14:41:41 -0300

    Esp lib release (v1.0)

--- Assets/EspLibrary.lua
+++ Assets/EspLibrary.lua
@@ -1,32 +1,155 @@
---[[
-    Horizon esp library | Its so pro
-    Credits: ic3w0lf and sirius sense library
-]]
-
-------------
 -- Variables
-------------
 
 local Players = game:GetService("Players")
 local RunService = game:GetService("RunService")
 
---------------
+local ViewportSize = workspace.CurrentCamera.ViewportSize
+
 -- Esp Object
---------------
 
 local EspLibrary = {}
 EspLibrary.__index = EspLibrary
 
-function EspLibrary.new(Object: Part)
-    local self = setmetatable({Object = Object, Renders = {}}, EspLibrary)
+function EspLibrary.new(Object, ParentObject, MaxDist)
+    local self = setmetatable({}, EspLibrary)
+    self.Settings = {}
+    self.RenderedObjects = {}
+    self.Object = Object
+    self.ParentObject = ParentObject
+    self.MaxDistance = MaxDist or math.huge
+    
+    self.EspRemoved = Instance.new("BindableEvent")
+    
+    if Players:GetPlayerFromCharacter(self.Object.Parent) then
+        self.IsPlayer = true
+        self.Player = Players:GetPlayerFromCharacter(self.Object.Parent)
+        self.PlayerCharacter = self.Player.Character
+        self.Humanoid = self.PlayerCharacter.Humanoid
+
+        self.Root = self.PlayerCharacter.HumanoidRootPart
+        self.Head = self.PlayerCharacter.Head
+    end
+
+    self.Connection = RunService.RenderStepped:Connect(function()
+        local WorldPosition, OnScreen = workspace.CurrentCamera:WorldToViewportPoint(self.Object.Position)
+        local OnRange = WorldPosition.Z < self.MaxDistance
+
+        if self.IsPlayer then
+            if not Players:FindFirstChild(self.Player.Name) or not self.PlayerCharacter or not self.Humanoid or self.Humanoid.Health == 0 then
+                self:Destruct()
+                return
+            end
+        else
+            if not self.Object or not self.Object.Parent then
+                self:Destruct()
+                return
+            end
+        end
+
+        if OnScreen and OnRange then
+            if self.RenderedObjects then
+                if self.RenderedObjects.TextEsp then
+                    for i,v in pairs(self.Settings.Text) do
+                        self.RenderedObjects.TextEsp[i] = v
+                    end
+
+                    self.RenderedObjects.TextEsp.Position = Vector2.new(WorldPosition.X, WorldPosition.Y) - Vector2.new(self.RenderedObjects.TextEsp.TextBounds.X / 2, (self.Settings.Text.PositionMode == "Top" and 0 or self.Settings.Text.PositionMode == "Middle" and self.RenderedObjects.TextEsp.TextBounds.Y / 2 or self.Settings.Text.PositionMode == "Bottom" and self.RenderedObjects.TextEsp.TextBounds.Y))
+                    self.RenderedObjects.TextEsp.Text = self.Settings.Text.Text:gsub("{name}", self.Object.Name):gsub("{distance}", WorldPosition.Z)
+                end
+
+                if self.RenderedObjects.Chams then
+                    for i,v in pairs(self.Settings.Chams) do
+                        self.RenderedObjects.Chams[i] = v
+                    end
+
+                    self.RenderedObjects.Chams.Adornee = (self.PlayerCharacter or self.ParentObject or self.Object)
+                end
+
+                if self.RenderedObjects.BoxEsp then
+                    for i,v in pairs(self.Settings.Box) do
+                        self.RenderedObjects.BoxOutline[i] = v
+                    end
+
+                    local BorderSettings = {
+                        Visible = self.Settings.Box.Visible,
+                        Color = Color3.new(0,0,0),
+                        Filled = false,
+                        Transparency = 1,
+                        Thickness = self.Settings.Box.Thickness + 2
+                    }
+
+                    for i,v in pairs(BorderSettings) do
+                        self.RenderedObjects.BoxEsp[i] = v
+                    end
+
+                    local RootPosition, RootVis = workspace.CurrentCamera:WorldToViewportPoint(self.Root.Position)
+                    local HeadPosition = workspace.CurrentCamera:WorldToViewportPoint(self.Head.Position + Vector3.new(0, 0.5, 0))
+                    local LegPosition = workspace.CurrentCamera:WorldToViewportPoint(self.Root.Position - Vector3.new(0, 3, 0))
+
+                    self.RenderedObjects.BoxEsp.Size = Vector2.new(ViewportSize.X / RootPosition.Z, HeadPosition.Y - LegPosition.Y)
+                    self.RenderedObjects.BoxEsp.Position = Vector2.new(RootPosition.X - self.RenderedObjects.BoxEsp.Size.X / 2, RootPosition.Y - self.RenderedObjects.BoxEsp.Size.Y / 2)
+
+                    self.RenderedObjects.BoxOutline.Position = Vector2.new(RootPosition.X - self.RenderedObjects.BoxOutline.Size.X / 2, RootPosition.Y - self.RenderedObjects.BoxOutline.Size.Y / 2)
+                    self.RenderedObjects.BoxOutline.Size = Vector2.new(ViewportSize.X / RootPosition.Z, HeadPosition.Y - LegPosition.Y)
+                    
+                end
+
+                if self.RenderedObjects.Tracer then
+                    for i,v in pairs(self.Settings.Tracer) do
+                        self.RenderedObjects.TracerOutline[i] = v
+                    end
+
+                    local BorderSettings = {
+                        Visible = self.Settings.Tracer.Visible,
+                        Color = Color3.new(0,0,0),
+                        Filled = false,
+                        Transparency = 1,
+                        Thickness = self.Settings.Tracer.Thickness + 2
+                    }
+
+                    for i,v in pairs(BorderSettings) do
+                        self.RenderedObjects.Tracer[i] = v
+                    end
+
+                    self.RenderedObjects.TracerOutline.From = (self.Settings.Tracer.TracerPosition == "Bottom" and Vector2.new(ViewportSize.X / 2, ViewportSize.Y)) or (self.Settings.Tracer.TracerPosition == "Middle" and ViewportSize * 0.5) or (self.Settings.Tracer.TracerPosition == "Top" and Vector2.new(ViewportSize.X / 2, 0)) or Vector2.new(ViewportSize.X / 2, ViewportSize.Y)
+                    self.RenderedObjects.TracerOutline.To = Vector2.new(WorldPosition.X, WorldPosition.Y)
+
+                    self.RenderedObjects.Tracer.Visible = self.Settings.Tracer.Outline
+                    self.RenderedObjects.Tracer.From = (self.Settings.Tracer.TracerPosition == "Bottom" and Vector2.new(ViewportSize.X / 2, ViewportSize.Y)) or (self.Settings.Tracer.TracerPosition == "Middle" and ViewportSize * 0.5) or (self.Settings.Tracer.TracerPosition == "Top" and Vector2.new(ViewportSize.X / 2, 0)) or Vector2.new(ViewportSize.X / 2, ViewportSize.Y)
+                    self.RenderedObjects.Tracer.To = Vector2.new(WorldPosition.X, WorldPosition.Y)
+                end
+            end
+        else
+            for _,object in pairs(self.RenderedObjects) do
+                if typeof(object) ~= "Instance" then
+                    object.Visible = false
+                end
+            end
+        end
+    end)
 
     return self
 end
 
-function EspLibrary:CreateChams(Settings)
-    assert(self.Object, string.format("[Horizon Esp Core] Common Error: self.Object is nil"))
-    local Connection
-    local NewSettings = {
+function EspLibrary:Destruct()
+    --# ugly part
+    if self.RenderedObjects.Chams then self.RenderedObjects.Chams:Destroy() end
+    if self.RenderedObjects.TextEsp then self.RenderedObjects.TextEsp:Remove() end
+    if self.RenderedObjects.BoxEsp then self.RenderedObjects.BoxEsp:Remove() end
+    if self.RenderedObjects.BoxOutline then self.RenderedObjects.BoxOutline:Remove() end
+    if self.RenderedObjects.Tracer then self.RenderedObjects.Tracer:Remove() end
+    if self.RenderedObjects.TracerOutline then self.RenderedObjects.TracerOutline:Remove() end
+    --# end of ugly part
+
+    self.EspRemoved:Fire(true)
+    self.Connection:Disconnect()
+    table.clear(self)
+end
+
+function EspLibrary:BuildChams(Settings)
+    self.RenderedObjects.Chams = Instance.new("Highlight")
+    self.RenderedObjects.Chams.Parent = (gethui and gethui()) or game:GetService("CoreGui")
+    self.Settings.Chams = {
         FillColor = Color3.fromRGB(255, 0, 0),
         FillTransparency = 0.5,
         OutlineColor = Color3.new(1,1,1),
@@ -36,47 +159,13 @@ function EspLibrary:CreateChams(Settings)
     }
 
     for i,v in pairs(Settings or {}) do
-        if not NewSettings[i] then return error(string.format('[Horizon Esp Core] Unknown property "%s"', i), 3) end
-        NewSettings[i] = (i == "DepthMode" and Enum.HighlightDepthMode[v] or v)
+        self.Settings.Chams[i] = v
     end
-
-    self.Renders["Chams"] = {Instance.new("Highlight"), NewSettings}
-    self.Renders["Chams"][1].Parent = (gethui and gethui() or game:GetService("CoreGui"))
-
-    function self.__DisconnectChams()
-        Connection:Disconnect()
-        self.Renders["Chams"][1]:Destroy()
-        self.Renders["Chams"] = nil
-    end
-
-    Connection = game:GetService("RunService").RenderStepped:Connect(function()
-        if self.Renders["Chams"] then
-            if self.Object ~= nil and self.Object.Parent ~= nil and (Players:GetPlayerFromCharacter(self.Object.Parent) ~= nil and Players:GetPlayerFromCharacter(self.Object.Parent).Character.Humanoid.Health > 0) or true then
-
-                for Prop, Value in next, self.Renders["Chams"][2] or {} do
-                    if self.Renders["Chams"][1][Prop] then self.Renders["Chams"][1][Prop] = (self.Renders["Chams"][1][Prop] == "DepthMode" and Enum.HighlightDepthMode[Value] or Value) end
-                end
-
-                if Players:GetPlayerFromCharacter(self.Object.Parent) then
-                    self.Renders["Chams"][1].Adornee = self.Object.Parent
-                else
-                    self.Renders["Chams"][1].Adornee = self.Object
-                end
-            else
-                self.__DisconnectChams()
-            end
-        else
-            Connection:Disconnect()
-        end
-    end)
-
-    return self.Renders["Chams"][1], self.Renders["Chams"][2]
 end
 
-function EspLibrary:CreateText(Settings)
-    assert(self.Object, string.format("[Horizon Esp Core] Common Error: self.Object is nil"))
-    local Connection
-    local NewSettings = {
+function EspLibrary:BuildText(Settings)
+    self.RenderedObjects.TextEsp = Drawing.new("Text")
+    self.Settings.Text = {
         Visible = true,
         Color = Color3.new(1,1,1),
         Transparency = 1,
@@ -86,153 +175,46 @@ function EspLibrary:CreateText(Settings)
         OutlineColor = Color3.new(0,0,0),
         Center = false,
         Font = 1,
-        MaxDistance = 150
     }
 
     for i,v in pairs(Settings or {}) do
-        if not NewSettings[i] then return error(string.format('[Horizon Esp Core] Unknown property "%s"', i), 3) end
-        NewSettings[i] = v
+        self.Settings.Text[i] = v
     end
-
-    self.Renders["Text"] = {Drawing.new("Text"), NewSettings}
-    
-
-    function self.__DisconnectText()
-        Connection:Disconnect()
-        self.Renders["Text"][1]:Remove()
-    end
-
-    Connection = game:GetService("RunService").RenderStepped:Connect(function()
-        if self.Renders["Text"] then
-            if self.Object ~= nil and self.Object.Parent ~= nil and (Players:GetPlayerFromCharacter(self.Object.Parent) ~= nil and Players:GetPlayerFromCharacter(self.Object.Parent).Character.Humanoid.Health > 0) or true then
-                for Prop, Value in next, self.Renders["Text"][2] or {} do
-                    if Prop ~= "MaxDistance" then
-                        self.Renders["Text"][1][Prop] = Value
-                    end
-                end
-
-                local WorldPosition = self.Object.Position
-                local Position, Onscreen = workspace.Camera:WorldToViewportPoint(WorldPosition)
-
-
-                if Onscreen and Position.Z < self.Renders["Text"][2].MaxDistance then
-                    self.Renders["Text"][1].Position = Vector2.new(Position.X, Position.Y) - Vector2.new(self.Renders["Text"][1].TextBounds.X / 2, self.Renders["Text"][1].TextBounds.Y / 2)
-                    self.Renders["Text"][1].Text = self.Renders["Text"][2].Text:gsub("{name}", self.Object.Name):gsub("{distance}", math.floor(Position.Z))
-                    self.Renders["Text"][1].Visible = self.Renders["Text"][2].Visible
-                else
-                    self.Renders["Text"][1].Visible = false
-                end
-
-            else
-                self.__DisconnectText()
-            end
-        else
-            Connection:Disconnect()
-        end
-    end)
-
-    return self.Renders["Text"][1], self.Renders["Text"][2]
 end
 
-function EspLibrary:CreateBox(Settings)
-    assert(self.Object, string.format("[Horizon Esp Core] Common Error: self.Object is nil"))
-    assert(Players:GetPlayerFromCharacter(self.Object.Parent), string.format("[Horizon Box Esp] Failed to connect box to %s, box esp is called from the object's parent for ease of use!", self.Object:GetFullName()))
-
-    local Connection
-    local NewSettings = {
+function EspLibrary:BuildBox(Settings)
+    assert(self.IsPlayer, "[Horizon Box Esp] Box esp cannot be used in a part!", 1)
+    self.RenderedObjects.BoxEsp = Drawing.new("Square")
+    self.RenderedObjects.BoxOutline = Drawing.new("Square")
+    self.Settings.Box = {
         Visible = true,
         Color = Color3.new(1,1,1),
         Filled = false,
         Transparency = 1,
-        Thickness = 1,
-        MaxDistance = 150
+        Thickness = 1
     }
-    
 
     for i,v in pairs(Settings or {}) do
-        if not NewSettings[i] then return error(string.format('[Horizon Esp Core] Unknown property "%s"', i), 3) end
-        NewSettings[i] = v
+        self.Settings.Box[i] = v
     end
+end
 
-    self.Renders["Box"] = {Drawing.new("Square"), NewSettings, Drawing.new("Square")}
-
-    local OldSettings = {
+function EspLibrary:BuildTracer(Settings)
+    self.RenderedObjects.Tracer = Drawing.new("Line")
+    self.RenderedObjects.TracerOutline = Drawing.new("Line")
+    self.Settings.Tracer = {
         Visible = true,
-        Color = Color3.new(0,0,0),
-        Filled = false,
+        Color = Color3.new(1,1,1),
         Transparency = 1,
-        Thickness = self.Renders["Box"][2].Thickness + 2
+        Thickness = 1,
+        TracerPosition = "Bottom",
+        From = Vector2.new(0.5, 1),
+        Outline = true
     }
-    
-    for i,v in pairs(OldSettings or {}) do
-        OldSettings[i] = v
-        self.Renders["Box"][3][i] = v
-    end
 
-    function self.__DisconnectBox()
-        Connection:Disconnect()
-        self.Renders["Box"][1]:Remove()
-        self.Renders["Box"][3]:Remove()
-    end
-
-    Connection = game:GetService("RunService").RenderStepped:Connect(function()
-        if self.Renders["Box"] then
-            if self.Object ~= nil and self.Object.Parent ~= nil and (Players:GetPlayerFromCharacter(self.Object.Parent) ~= nil and Players:GetPlayerFromCharacter(self.Object.Parent).Character.Humanoid.Health > 0) or true then
-                for Prop, Value in next, self.Renders["Box"][2] or {} do
-                    if Prop ~= "MaxDistance" then
-                        self.Renders["Box"][3][Prop] = Value
-                    end
-                end
-
-                for i,v in pairs(OldSettings or {}) do
-                    OldSettings[i] = v
-                    self.Renders["Box"][1][i] = v
-                end
-
-                local WorldPosition = self.Object.Position
-                local Position, Onscreen = workspace.Camera:WorldToViewportPoint(WorldPosition)
-                local ViewportSize = workspace.CurrentCamera.ViewportSize
-                local Player = Players:GetPlayerFromCharacter(self.Object.Parent)
-
-                if not Player or not Player.Character then
-                    self.__DisconnectBox()
-                end
-
-                local Char = Player.Character
-                local Head, Root = Char.Head, Char.HumanoidRootPart
-                local RootPosition, RootVis = workspace.Camera:WorldToViewportPoint(Root.Position)
-                local HeadPosition = workspace.CurrentCamera:WorldToViewportPoint(Head.Position + Vector3.new(0, 0.5, 0))
-                local LegPosition = workspace.CurrentCamera:WorldToViewportPoint(Root.Position - Vector3.new(0, 3, 0))
-
-                if Onscreen and Position.Z < self.Renders["Box"][2].MaxDistance then
-                    self.Renders["Box"][3].Size = Vector2.new(ViewportSize.X / RootPosition.Z, HeadPosition.Y - LegPosition.Y)
-                    self.Renders["Box"][3].Position = Vector2.new(RootPosition.X - self.Renders["Box"][3].Size.X / 2, RootPosition.Y - self.Renders["Box"][3].Size.Y / 2)
-                    self.Renders["Box"][3].Visible = self.Renders["Box"][2].Visible
-
-                    self.Renders["Box"][1].Size = Vector2.new(ViewportSize.X / RootPosition.Z, HeadPosition.Y - LegPosition.Y)
-                    self.Renders["Box"][1].Position = Vector2.new(RootPosition.X - self.Renders["Box"][1].Size.X / 2, RootPosition.Y - self.Renders["Box"][1].Size.Y / 2)
-                    self.Renders["Box"][1].Visible = self.Renders["Box"][2].Visible
-                else
-                    self.Renders["Box"][1].Visible = false
-                    self.Renders["Box"][3].Visible = false
-                end
-            else
-                self:__DisconnectBox()
-            end
-        else
-            Connection:Disconnect()
-        end
-    end)
-
-    return self.Renders["Box"][1], self.Renders["Box"][2]
-end
-
-for _,v in pairs(game.Players:GetPlayers()) do
-    local playerCharacter = v.Character
-    if (v and v.Character) ~= nil and playerCharacter:FindFirstChild("HumanoidRootPart") then
-        local EspObject = EspLibrary.new(playerCharacter.HumanoidRootPart)
-        EspObject:CreateBox()
-        EspObject:CreateText({Text = string.format("%s (%s)", v.DisplayName, v.Name)})
-        EspObject:CreateChams()
+    for i,v in pairs(Settings or {}) do
+        self.Settings.Tracer[i] = v
     end
 end
+
+return EspLibrary
