ba08c4953ec2e11173bebaeb6c7e8aead5de4181
Rain <94387725+RainPGN@users.noreply.github.com>
2023-08-21 14:35:57 -0300

    Update UILibrary.lua

--- Libraries/UILibrary.lua
+++ Libraries/UILibrary.lua
@@ -489,7 +489,7 @@ function Horizon:CreateNewGui(func)
             end)
 
             function ComponentFunctions:Toggle()
-                if Configuration.Active == (false or nil) then
+                if not Configuration.Active or Configuration.Active == false then
                     Configuration.Active = true
 
                     Horizon:PlayTween(TweenService:Create(Toggle.Main.Inner, TweenInfo.new(0.6, Enum.EasingStyle.Quint), {Position = UDim2.new(0, 20, 0, 1)}))
